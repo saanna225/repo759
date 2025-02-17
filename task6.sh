7485e42f77da2f41bfa5a04f063e4e42e570461f
saanna225 <sdiwakar@wisc.edu>
2025-02-16 20:54:49 -0600

    Delete task6.sh

--- task6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=Task6
-#SBACTH --output=task6.err
-#SBATCH --output=task6.out
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-
-# Print the hostname of the machine
-hostname
